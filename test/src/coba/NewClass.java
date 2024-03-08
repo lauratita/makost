5d5ab2f6ce237d6119287466e599bb49b72de9a8
itanrll <itanur246@gmail.com>
2024-03-08 09:29:45 +0700

    variable

--- test/src/coba/NewClass.java
+++ test/src/coba/NewClass.java
@@ -12,7 +12,7 @@ package coba;
 public class NewClass {
     public static void main(String[] args) {
         
-        System.out.println("Hello");
+        int a;
         System.out.println("Hello Laura");
     }
 }
