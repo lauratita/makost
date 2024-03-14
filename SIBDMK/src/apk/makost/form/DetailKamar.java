496e006a950d2dd84ab45c6364f56b558ec3de69
Laura Tita <lauratita316@gmail.com>
2024-03-14 17:14:34 +0700

    menambahkan icon pada Kamar.java dan ManajemenKamar.java

--- SIBDMK/src/apk/makost/form/DetailKamar.java
+++ SIBDMK/src/apk/makost/form/DetailKamar.java
@@ -8,12 +8,12 @@ package apk.makost.form;
  *
  * @author Neendy's
  */
-public class DataKamar extends javax.swing.JInternalFrame {
+public class DetailKamar extends javax.swing.JInternalFrame {
 
     /**
-     * Creates new form DataKamar
+     * Creates new form DetailKamar
      */
-    public DataKamar() {
+    public DetailKamar() {
         initComponents();
     }
 
@@ -26,15 +26,22 @@ public class DataKamar extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/apk/makost/form/DetailKamar.png"))); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1496, Short.MAX_VALUE)
+            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 836, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -42,5 +49,6 @@ public class DataKamar extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
--- SIBDMK/src/apk/makost/main/Main.java
+++ SIBDMK/src/apk/makost/main/Main.java
@@ -111,7 +111,7 @@ public class Main extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 769, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 769, Short.MAX_VALUE)
         );
 
         pack();
